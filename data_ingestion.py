8709793d7b8de2d32c6c45a026ad76c73e6e1680
paishrikrishna <2017.shrikrishna.pai@ves.ac.in>
2023-09-22 15:05:14 +0530

    Added Historical Load and its sci script

--- data_ingestion.py
+++ data_ingestion.py
@@ -7,28 +7,41 @@ import pandas as pd
 
 
 
+def one_m_hist_load(symbol):
+    start_date = datetime.now() - timedelta(days=30)
+    end_date = start_date + timedelta(days=7)
+    c = 0
+    while end_date <= datetime.now() + timedelta(days=10):
+        c+=1
+        print(f'Fetching Data from {start_date} - {end_date}')
+        historical_data(start_date,end_date,symbol,c)
+        start_date = end_date
+        end_date = start_date + timedelta(days=7)
+
 
 def hist_load():
-    start_date = datetime(2002, 8, 12)
-    end_date = datetime(2023, 9, 22)
+    start_date = datetime.now() - timedelta(days=7)
+    end_date =  datetime.now()  
     print(f'{start_date} - {end_date}')
     historical_data(start_date,end_date)
     
 
 
-def historical_data(start_date,end_date):
-    data = yf.download('TCS.NS', start=start_date, end=end_date)
-    data.to_csv('TCS.csv',sep = ',',index=True)
+def historical_data(start_date,end_date,symbol,c):
+    data = yf.download(symbol, start=start_date, end=end_date,interval='1m')
+    if c == 1:
+        data.to_csv(f"{symbol.replace('.','_')}.csv",sep = ',',index=True,mode='a')
+    else:
+        data.to_csv(f"{symbol.replace('.','_')}.csv",sep = ',',index=True,mode='a',header=None)
     
 
-def inc_load():
-    symbl = 'AAPL'
+def inc_load(symbl):
     while True:
         now = datetime.now()
         price = stock_info.get_live_price("AAPL")
         print(now, f'{symbl}:', price)
         data = {
-                'Time':now,
+                'Datetime':now,
                 'Symbl': f'{symbl}',
                 'Price': price
                 }
@@ -40,7 +53,8 @@ def inc_load():
 
 
 def main():
-    inc_load()
+    symbol = 'TCS.NS'
+    one_m_hist_load(symbol)
 
 if __name__ == '__main__':
     main()
